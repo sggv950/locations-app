3af47bb795e9572fe3a6019a78dd7810e10f8fdd
sggv950 <38014323+sggv950@users.noreply.github.com>
2021-08-17 17:10:02 +0300

    commit app

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 import './header.scss';
 
 export const Header = () => {
   return (
     <header className='app-header'>
       <h2>Locations App</h2>
-      <FontAwesomeIcon icon={faCodeBranch} size="lg" />
+      <a href='https://github.com/sggv950/wix-editor-locations-app' target="_blank">
+        <FontAwesomeIcon icon={faCodeBranch} size='lg' />
+      </a>
     </header>
   );
 };
